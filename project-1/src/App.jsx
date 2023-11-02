ba54edf0961ba767e55d399cbab10732d324fa90
atharva0078 <atharvapuranik07@gmail.com>
2023-11-02 17:14:28 +0530

    Initial commit

---
+++ project-1/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+
+
+function App()
+{
+
+
+  return (
+
+    <div>
+      <nav>
+        <div className="Logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+  )
+}
+
+export default App
